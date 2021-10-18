f359cde290589ef818075064b7d567ba6177fa8a
Mahashweta Patra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-10-18 15:52:43 -0500

    Add files via upload

--- PCA_Assets.m
+++ PCA_Assets.m
@@ -5,8 +5,7 @@
 %        - 7th Sep, Principal component analysis is done,
 %        -07 Oct, saving data and figure to diferent folder   
 %=========================================================================
-function CoeffMatrix=PCA_Assets(assettype,year,filename,Array);
-% T1=1:1:24; %Time steps
+function CoeffMatrix=PCA_Assets(filename,Array);
 SizeScenario=size(Array);
 b_array=[];
 for k=3:SizeScenario(1)
@@ -20,23 +19,6 @@ for k=3:SizeScenario(1)
         b_array(k,i)=b(i);
     end
 end
-%[coeff,score,latent, tsquared, explained]=pca(b_array);
 [coeff]=pca(b_array);
 CoeffMatrix=[coeff(:,1);coeff(:,2);coeff(:,3);coeff(:,4)];
-if assettype==1
-    Fn=strcat('Coefficients/solar_coefficient',num2str(year),'.csv');
-    fid = fopen(Fn,'a+');
-    fprintf(fid,'%d\n',CoeffMatrix);
-    fclose(fid);   
-elseif assettype==2
-    Fn=strcat('Coefficients/wind_coefficient',num2str(year),'.csv');
-    fid = fopen(Fn,'a+');
-    fprintf(fid,'%d\n',CoeffMatrix);
-    fclose(fid);  
-elseif assettype==3
-    Fn=strcat('Coefficients/load_coefficient',num2str(year),'.csv');
-    fid = fopen(Fn,'a+');
-    fprintf(fid,'%d\n',CoeffMatrix);
-    fclose(fid); 
-end
 end
