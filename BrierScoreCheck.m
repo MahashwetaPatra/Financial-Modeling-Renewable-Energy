de9e72f848fb93f125fba993ce175fec9e7c873f
Mahashweta Patra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-11-24 21:21:04 -0600

    Add files via upload
    Calls all the wind assets for intraday 1,2,3,4 and for all the wind assets calculates the Brier score for the max wind generation and with the Brier scores shows histogram plot

--- BrierScoreCheck.m
+++ BrierScoreCheck.m
@@ -1,47 +1,40 @@
 %
-% NOTE:    calculates the Brier Score for the asset for intra day 1, 2, 3, 4
+% NOTE:    function that calculates the Brier Score for the  max wind generation
 %
 % HIST:  - 11 Aug, 2021: Created by Patra
-%         
+%         -Added more notes to it
 %=========================================================================
-function BrierScoreCheck(filename);
-BrierScoreSeries=[];NumberSeries=[];
-for day=1:4
-    BrierScore=0.0;number=0.0;
-    datetime.setDefaultFormats('defaultdate','yyyyMMdd')
-    t = datetime(2017,1,1:365);
-    date=t';
-    time=char(date(1));
-    for k=1:365
-        year=char(date(k));
-        name=strcat('Scovilleriskpartners/CSV/IntraDay',num2str(day),'/SimDat_',year,'/wind/',string(filename));
-        Array = readtable(name);
-        score=0.0;
-        for column=1:6
-            c=column+2;
-            c1=Array{4:1003,c}; 
-            n = length(c1);
-            idx=c1==0;
-            out=sum(idx(:));
-            probability=out/n;
-            if probability>0.01
-                actual=Array{3,c};
-                if actual==0
-                    BS=(probability-1)*(probability-1);
-                else
-                    BS=(probability-0)*(probability-0);
-                end
-                score=score+BS;
-                number=number+1;
+function BrierScore=BrierScoreCheck(filename,day);
+BrierScore=0.0;number=0.0;
+score=0.0;
+datetime.setDefaultFormats('defaultdate','yyyyMMdd')
+t = datetime(2017,1,1:730);
+date=t';
+time=char(date(1));
+for k=1:730
+    year=char(date(k));
+    name=strcat('Scovilleriskpartners/CSV/IntraDay',num2str(day),'/SimDat_',year,'/wind/',string(filename));
+    %name=strcat('Scovilleriskpartners/CSV/DA/SimDat_',year,'/wind/',string(filename));
+    Array = readtable(name);
+    for column=1:6 % For DA the value is 24
+        c=column+2;
+        c1=Array{4:1003,c};
+        check=max(c1);% considering the max wind generation for each hour
+        n = length(c1);
+        idx=c1==check;% checking whether the highest value repeats or not 
+        out=sum(idx(:));
+        probability=out/n;
+        if probability>0.01% checking the frequency of occurence of the highest value
+            actual=Array{3,c};
+            if actual==check
+                BS=(probability-1)*(probability-1);
+            else
+                BS=(probability-0)*(probability-0);
             end
+            score=score+BS;
+            number=number+1;
         end
-        BrierScore=BrierScore+score;
     end
-    BrierScore;
-    number;
-    BrierScoreSeries=[BrierScoreSeries;BrierScore/number];
-    NumberSeries=[NumberSeries;number];
 end
-BrierScoreSeries
-NumberSeries
+BrierScore=score/number;
 end
--- MaxWindGeneration.m
+++ MaxWindGeneration.m
@@ -1,7 +1,7 @@
 %
-% NOTE:    Checks for the asset from the metadata file which hits the maximum wind capacity and for that asset calls the 
-% function BrierScoreCheck and then for intraday 1,2,3,4 it calculates the four Brier scores.
-%
+% NOTE: Calls all the wind assets for intraday 1,2,3,4 and for all the wind assets 
+%       calculates the Brier score for the max wind generation and 
+%       with the Brier scores shows histogram plot
 % HIST:  - 13 Nov, 2021: Created by Patra
 %         
 %=========================================================================
@@ -9,8 +9,18 @@ tic
 clc;close all; clear all;
 Array = readtable('Scovilleriskpartners/CSV/metaData.xlsx');
 column=Array{38:152,8};
-[A,I]=sort(column);
-file=I(length(I)-1);
-FileName=Array{file,2}
-BrierScoreCheck(FileName)
+for day=1:4
+    Series=[];
+    for i=38:151
+        FileName=Array{i,2};
+        BrierScore=BrierScoreCheck(FileName,day);
+        Series=[Series;BrierScore];
+    end
+    figure(1)
+    subplot(1,5,day)
+    histogram(Series,20)
+    xlim([0 0.18])
+    ylim([0 20])
+    title(strcat('Intraday',num2str(day)))
+end
 toc
