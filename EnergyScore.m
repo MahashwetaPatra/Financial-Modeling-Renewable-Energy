b24e53a5dd1a812b96bac61f5200771eda526916
Mahashweta Patra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-12-05 11:00:28 -0600

    Add files via upload

--- EnergyScore.m
+++ EnergyScore.m
@@ -5,43 +5,35 @@
 %         
 %=========================================================================
 tic
-clc;close all; clear all;
+clc;close all;
 datetime.setDefaultFormats('defaultdate','yyyy-MM')
 t = datetime(2017,1,1:31:730);
 date1=t';% generating the xticks at every month for 2017 & 2018
-Array = readtable('Output/ESIntraDay1/Asset/ES_Scoville_All.csv');% calls all the assets from a folder
+for day=1:4
+Array = readtable(strcat('Output/ES/ESIntraDay',num2str(day),'/Asset/ES_Scoville_All.csv'));% calls all the assets from a folder
 date=1:1:730;% plotting the energy scores against days
 loadES=Array{:,2}; % considering the Load energy score
 solarES=Array{:,3}; % considering the Solar energy score
 windES=Array{:,4}; % considering the Wind energy score
 allES=Array{:,5}; % considering the overall energy score
-fig=figure(1);
+figure(1);% plots the energy score for solar
+subplot(4,1,day)
 set(gca, 'GridLineStyle', ':') %dotted grid lines
 set(gca,'FontSize',14,'LineWidth',2.75)
-xlim([1, 730]);
-subplot(4,1,1)% plots the energy score for load
-plot(date,loadES,'.-b')
-ylabel('Load ES')
-xlim([1, 730]);
-grid on;
-set(gca,'xticklabel',{[]})
-subplot(4,1,2)% plots the energy score for solar
 plot(date,solarES,'.-b')
 ylabel('Solar ES')
 xlim([1, 730]);
 grid on;
 set(gca,'xticklabel',{[]})
-subplot(4,1,3)% plots the energy score for wind
+
+figure(2);% plots the energy score for wind
+subplot(4,1,day)
 plot(date,windES,'.-b')
 ylabel('Wind ES')
 xlim([1, 730]);
 grid on;
-set(gca,'xticklabel',{[]})
-
-subplot(4,1,4)% plots the energy score for All
-plot(date,allES,'.-b')
-ylabel('All ES')
 xticks(1:31:730);
 year=char(date1);
 xlim([1, 730]);
 xticklabels(num2str(year));
+end
--- EnergyScoreZonalYear.m
+++ EnergyScoreZonalYear.m
@@ -6,43 +6,41 @@
 %         
 %=========================================================================
 tic
-clc;close all; clear all;
+clc;close all; 
 datetime.setDefaultFormats('defaultdate','yyyy-MM')
 t = datetime(2017,1,1:31:730);
 date1=t';% generating the xticks at every month for 2017 & 2018
 zones{1}='East';zones{2}='Coast';zones{3}='Far_West';zones{4}='North';
 zones{5}='North_Central';zones{6}='South';zones{7}='South_Central';zones{8}='West';
-files=dir('Output/ES/ESIntraDay1Zonal/Zonal/*.csv')
+files=dir('Output/ES/ESIntraDay1Zonal/Zonal/*.csv');
 for j=1:8
-for i=1:3
-
-Array = readtable(strcat('Output/ES/ESIntraDay',num2str(i),'Zonal/Zonal/ES_Scoville_',zones{j}));
-date=1:1:730;% plotting the energy scores against days
-loadES=Array{:,2}; % considering the Load energy score
-solarES=Array{:,3}; % considering the Solar energy score
-windES=Array{:,4}; % considering the Wind energy score
-allES=Array{:,5}; % considering the overall energy score
-
-figure(1);% plots the energy score for wind
-subplot(8,1,j)% plots the energy score for wind
-plot(date,solarES,'.-')
-hold on;
-ylabel(strcat('solar-',zones(j)))
-grid on;
-xticks(1:31:730);
-year=char(date1);
-xlim([1, 730]);
-xticklabels(num2str(year));
-
-figure(2);% plots the energy score for wind
-subplot(8,1,j)% plots the energy score for wind
-plot(date,windES,'.-')
-hold on;
-ylabel(strcat('wind-',zones(j)))
-grid on;
-xticks(1:31:730);
-year=char(date1);
-xlim([1, 730]);
-xticklabels(num2str(year));
-end
+    for i=1:4
+        Array = readtable(strcat('Output/ES/ESIntraDay',num2str(i),'Zonal/Zonal/ES_Scoville_',zones{j}));
+        %Array = readtable(strcat('Output/ES/ESDAZonal/Zonal/ES_Scoville_',zones{j}));
+        date=1:1:730;% plotting the energy scores against days
+        loadES=Array{:,2}; % considering the Load energy score
+        solarES=Array{:,3}; % considering the Solar energy score
+        windES=Array{:,4}; % considering the Wind energy score
+        allES=Array{:,5}; % considering the overall energy score
+        figure(1);% plots the energy score for wind
+        subplot(8,1,j)% plots the energy score for wind
+        plot(date,solarES,'.-')
+        hold on;
+        ylabel(strcat('solar-',zones(j)))
+        grid on;
+        xticks(1:31:730);
+        year=char(date1);
+        xlim([1, 730]);
+        xticklabels(num2str(year));
+        figure(2);% plots the energy score for wind
+        subplot(8,1,j)% plots the energy score for wind
+        plot(date,windES,'.-')
+        hold on;
+        ylabel(strcat('wind-',zones(j)))
+        grid on;
+        xticks(1:31:730);
+        year=char(date1);
+        xlim([1, 730]);
+        xticklabels(num2str(year));
+    end
 end
