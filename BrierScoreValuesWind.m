d89814d3e1e7f90380d503ac4aec29a72f05d2c4
Mahashweta Patra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-12-10 11:56:51 -0600

    Add files via upload
    Notes:  Calls all the assets, and for each assets calculates the Brier scores for the event of zero wind generation plot the histograms for the Brier score.

--- BrierScoreValuesWind.m
+++ BrierScoreValuesWind.m
@@ -1,29 +1,31 @@
 %
 % NOTE:    Calls all the assets, and for each assets calculates the Brier scores for the event
-% of zero wind generation plot the histograms for the KS score in figure 1
+% of zero wind generation plot the histograms for the Brier score in figure 1
 %          
 %
 % HIST:  - 15 Nov, 2021: Created by Patra
 %         
 %=========================================================================
-function scorevalue=BrierScoreValuesWind;
-%tic
-scorevalue=[];
-for day=1:4
-    BrierScoreSeries=[];
-    files = dir('Scovilleriskpartners/CSV/IntraDay1/SimDat_20170101/wind/*.csv');
-    for i=1:length(files)
-        BrierScore=0.0;number=0.0;score=0.0;
+tic
+clc;close all; clear all;
+files = dir('Scovilleriskpartners/CSV/IntraDay1/SimDat_20170101/wind/*.csv');
+scorevalue=zeros(length(files),4);
+for day=1:5
+    parfor i=1:length(files)
+        number=0.0;score=0.0;
         datetime.setDefaultFormats('defaultdate','yyyyMMdd')
-        t = datetime(2017,1,1:365);
+        t = datetime(2017,1,1:30);
         date=t';
-        time=char(date(1));
         filename=files(i).name;
-        for k=1:365
+        for k=1:30
             year=char(date(k));
-            name=strcat('Scovilleriskpartners/CSV/IntraDay',num2str(day),'/SimDat_',year,'/wind/',filename);
-            %name=strcat('Scovilleriskpartners/CSV/DA/SimDat_',year,'/wind/',filename);
-            Array = readtable(name);
+             if day==5
+                 name=strcat('Scovilleriskpartners/CSV/DA/SimDat_',year,'/wind/',filename);
+             
+             else
+                name=strcat('Scovilleriskpartners/CSV/IntraDay',num2str(day),'/SimDat_',year,'/wind/',filename);
+             end
+                Array = readtable(name);
             for column=1:6 %considers all the six hours
                 c=column+2;
                 c1=Array{4:1003,c}; % considers column for each hour 
@@ -43,18 +45,21 @@ for day=1:4
                 end
             end
         end
+        number;
         BrierScore=score/number;
-        BrierScoreSeries=[BrierScoreSeries;BrierScore];%averaging over
+        scorevalue(i,day)=BrierScore;
     end
-scorevalue(:,day)=BrierScoreSeries;% scorevalue shows Brier score for all intra days and plots
 end
-for day=1:4
-figure(1)
-subplot(2,2,day)
-histogram(scorevalue(:,day),20)
-xlim([0,0.25])
-ylim([0,65])
-title(strcat('Intra Day',num2str(day)))
+for day=1:5
+    figure(1)
+    subplot(1,5,day)
+    histogram(scorevalue(:,day),10)
+    if day==5
+        title("DA")
+    else
+        title(strcat('Intra Day',num2str(day)))
+    end
+    %xlim([0,0.25])
+    %ylim([0,65])   
 end
-%toc
-end
+toc
