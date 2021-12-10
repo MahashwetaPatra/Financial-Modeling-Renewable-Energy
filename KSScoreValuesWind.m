82f13fee0e4658c1245942f82eee765af62fd214
Mahashweta Patra <89363530+MahashwetaPatra@users.noreply.github.com>
2021-12-10 09:37:34 -0600

    Add files via upload

--- KSScoreValuesWind.m
+++ KSScoreValuesWind.m
@@ -6,57 +6,29 @@
 % HIST:  - 15 Nov, 2021: Created by Patra
 %         
 %=========================================================================
-
-function IntraDayKSAll=KSScoreValuesWind;
-IntraDayKS=[]; IntraDayKS2=[];IntraDayKS3=[]; IntraDayKS4=[];
-for Assetnumber=1:115 %calls all the assets from intraday 1,2,3 & 4
-    column=Assetnumber+1;
-    Array = readtable('Output/Percentiles/IntraDayNew1/Asset/Percentiles_Scoville_wind.csv');% calls all the assets from a folder
-    file=Array{:,column};
-    [h,p,ksstat,cv] = kstest(file,[file unifcdf(file,0,100)]);
-    IntraDayKS=[IntraDayKS;h p ksstat cv];
-    
-    Array = readtable('Output/Percentiles/IntraDayNew2/Asset/Percentiles_Scoville_wind.csv');% calls all the assets from a folder
-    file=Array{:,column};
-    [h,p,ksstat,cv] = kstest(file,[file unifcdf(file,0,100)]);
-    IntraDayKS2=[IntraDayKS2;h p ksstat cv];
-
-    Array = readtable('Output/Percentiles/IntraDayNew3/Asset/Percentiles_Scoville_wind.csv');% calls all the assets from a folder
-    file=Array{:,column};
-    [h,p,ksstat,cv] = kstest(file,[file unifcdf(file,0,100)]);
-    IntraDayKS3=[IntraDayKS3;h p ksstat cv];
-
-    Array = readtable('Output/Percentiles/IntraDayNew4/Asset/Percentiles_Scoville_wind.csv');% calls all the assets from a folder
-    file=Array{:,column};
-    [h,p,ksstat,cv] = kstest(file,[file unifcdf(file,0,100)]);
-    IntraDayKS4=[IntraDayKS4;h p ksstat cv];
-
+tic
+IntraDayKS=zeros(115,4);
+for day= 1:5
+    parfor Assetnumber=1:115 %calls all the assets from intraday 1,2,3 & 4
+        column=Assetnumber+1;
+        if day==5
+            Array = readtable('Output/Percentiles/DA/Asset/Percentiles_Scoville_wind.csv');% calls all the assets from a folder
+        else
+            Array = readtable(strcat('Output/Percentiles/IntraDayNew',num2str(day),'/Asset/Percentiles_Scoville_wind.csv'));% calls all the assets from a folder
+        end
+        file=Array{:,column};
+        [h,p,ksstat,cv]  = kstest(file,[file unifcdf(file,0,100)]);
+        IntraDayKS(Assetnumber,:)=[h,p,ksstat,cv] ;
+    end  
+    figure(2);
+    subplot(1,5,day)
+    histogram(IntraDayKS(:,3),20)
+    %xlim([0,0.3])
+    %ylim([0,40])
+    if day==5
+        title('DA')
+    else
+        title(strcat('Intra Day',num2str(day)))
+    end
 end
-
-IntraDayKSAll=[IntraDayKS(:,3) IntraDayKS2(:,3) IntraDayKS3(:,3) IntraDayKS4(:,3)];
-figure(2);
-subplot(2,2,1)
-histogram(IntraDayKS(:,3),20)
-xlim([0,0.3])
-ylim([0,40])
-title('Intra Day 1')
-
-subplot(2,2,2)
-histogram(IntraDayKS2(:,3),20)
-xlim([0,0.3])
-ylim([0,40])
-title('Intra Day 2')
-
-subplot(2,2,3)
-histogram(IntraDayKS3(:,3),20)
-xlim([0,0.3])
-ylim([0,40])
-title('Intra Day 3')
-
-subplot(2,2,4)
-histogram(IntraDayKS4(:,3),20)
-xlim([0,0.3])
-ylim([0,40])
-title('Intra Day 4')
-
-end
+toc
